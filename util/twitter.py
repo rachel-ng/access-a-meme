81e2ad65dd9cb3e387f1b0790a1139b811976c7c
Sophia Xia <sophia.xia67@myhunter.cuny.edu>
2020-11-21 00:06:51 -0500

    stop waiting

--- util/twitter.py
+++ util/twitter.py
@@ -11,7 +11,7 @@ ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
 def connect_to_twitter():
 	auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY);
 	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
-	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+	api = tweepy.API(auth, wait_on_rate_limit=False, wait_on_rate_limit_notify=True)
 	return api
 
 def get_timeline():
